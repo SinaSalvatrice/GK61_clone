776b8293a582f621d6b71a7056ca121136f30718
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-04-14 18:26:20 +0200

    k

--- config.h
+++ config.h
@@ -8,6 +8,6 @@
 #define MATRIX_COLS 14
 #define MATRIX_ROW_PINS { 2, 3, 4, 5, 6 }
 #define MATRIX_COL_PINS { 7, 8, 9, 10, 11, 12, 13, 14, 15, 26, 27, 28, 29 }
-#define DIODE_DIRECTION COL2ROW
+#define DIODE_DIRECTION ROW2COL
 
 #endif // CONFIG_H
