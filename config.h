0511652c573721f9825af58d7a3b839df86e2f80
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-04-20 00:31:15 +0200

    Update config.h

--- config.h
+++ config.h
@@ -4,9 +4,13 @@
 #define WS2812_DI_PIN GP29
 
 
+
 #define WS2812_DI_PIN GP29
 #define RGB_MATRIX_LED_COUNT 36
 #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 120
 #define RGB_MATRIX_SLEEP
+
+// Enable all standard RGB Matrix effects for VIA
+#define RGB_MATRIX_EFFECTS
 
 #endif // CONFIG_H
