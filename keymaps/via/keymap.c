c1d69a48ec7113e527bc82191a3649e214256ac0
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-04-19 20:15:23 +0200

    via fix?

--- keymaps/via/keymap.c
+++ keymaps/via/keymap.c
@@ -1,9 +1,5 @@
 #include QMK_KEYBOARD_H
 
-// Keymap for GK61 clone with RGB Matrix support
-// Rows: 0, 1, 2, 3, 4
-// Cols: 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 26, 27, 28
-
 enum layer_names {
     _BASE,
     _FN,
@@ -16,8 +12,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_60_ansi(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT         ,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,        ,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, FN,      KC_APP,  KC_RCTL
     ),
 
@@ -29,5 +25,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            QK_LOCK,                            _______, _______, _______, CAD
     )
 };
-
-
